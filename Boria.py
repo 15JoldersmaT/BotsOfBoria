b8a8b35c211097e7da3e0a0e5b08f7f0fed69dff
15JoldersmaT <51901299+15JoldersmaT@users.noreply.github.com>
2024-02-02 11:29:06 -0500

    Update Boria.py

--- Boria.py
+++ Boria.py
@@ -13,11 +13,12 @@ time = 0
 grav = 1
 
 # Generate random counts for bots of each color
-pinkBots = random.randint(1, 45) 
-yellowBots = random.randint(1, 45) 
-greenBots = random.randint(1, 45) 
-blueBots = random.randint(1, 45) 
-
+pinkBots = random.randint(1, 25) 
+yellowBots = random.randint(1, 25) 
+greenBots = random.randint(1, 25) 
+blueBots = random.randint(1, 25) 
+orangeBots = random.randint(1, 25)  # Add orange bots
+cyanBots = random.randint(1, 25)    # Add cyan bots
 
 g1 = random_number = random.uniform(-1, 1)
 g2 = random_number = random.uniform(-1, 1)
@@ -37,17 +38,45 @@ g14 = random_number = random.uniform(-1, 1)
 g15 = random_number = random.uniform(-1, 1)
 g16 = random_number = random.uniform(-1, 1)
 
+g10 = random_number = random.uniform(-1, 1)
+g11 = random_number = random.uniform(-1, 1)
+g12 = random_number = random.uniform(-1, 1)
+g13 = random_number = random.uniform(-1, 1)
+g14 = random_number = random.uniform(-1, 1)
+g15 = random_number = random.uniform(-1, 1)
+g16 = random_number = random.uniform(-1, 1)
+
+g17 = random_number = random.uniform(-1, 1)
+g18 = random_number = random.uniform(-1, 1)
+g19 = random_number = random.uniform(-1, 1)
+g20 = random_number = random.uniform(-1, 1)
+g21 = random_number = random.uniform(-1, 1)
+g22 = random_number = random.uniform(-1, 1)
+g23 = random_number = random.uniform(-1, 1)
+g24= random_number = random.uniform(-1, 1)
+g25 = random_number = random.uniform(-1, 1)
+g26 = random_number = random.uniform(-1, 1)
+g27 = random_number = random.uniform(-1, 1)
+g28 = random_number = random.uniform(-1, 1)
+g29 = random_number = random.uniform(-1, 1)
+g30 = random_number = random.uniform(-1, 1)
+g31 = random_number = random.uniform(-1, 1)
+g32 = random_number = random.uniform(-1, 1)
+g33 = random_number = random.uniform(-1, 1)
+g34 = random_number = random.uniform(-1, 1)
+g35 = random_number = random.uniform(-1, 1)
+g36 = random_number = random.uniform(-1, 1)
+
+
 bots = []
 
 class Bot:
-    def __init__(self,x, y,color):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
         
  
-
-            
 
     def move(self):
         global bots
@@ -67,7 +96,26 @@ class Bot:
         global g14
         global g15
         global g16
-
+        global g17
+        global g18
+        global g19
+        global g20
+        global g21
+        global g22
+        global g23
+        global g24
+        global g25
+        global g26
+        global g27
+        global g28
+        global g29
+        global g30
+        global g31
+        global g32
+        global g33
+        global g34
+        global g35
+        global g36
         ##SET BOUNDS##
 
         if self.x < 15:
@@ -106,37 +154,78 @@ class Bot:
                     g = g2
                 elif self.color == (0,0,255):
                     g = g3
-                else:
+                elif self.color == (255,165,0):  # Orange color
                     g = g4
-
+                elif self.color == (0,255,255):  # Cyan color
+                    g = g5
+                else:
+                    g = g6
             elif i.color == (0,255,0):
                 if self.color ==(255,0,0):
-                    g = g5
-                elif self.color == (0,255,0):
-                    g = g6
-                elif self.color == (0,0,255):
                     g = g7
-                else:
-                    g = g8
-            elif i.color == (0,0,255):
-                if self.color ==(255,0,0):
-                    g = g9
                 elif self.color == (0,255,0):
-                    g = g10
+                    g = g8
                 elif self.color == (0,0,255):
+                    g = g9
+                elif self.color == (255,165,0):
+                    g = g10
+                elif self.color == (0,255,255):
                     g = g11
                 else:
                     g = g12
-            else:
+            elif i.color == (0,0,255):
                 if self.color ==(255,0,0):
                     g = g13
                 elif self.color == (0,255,0):
-                    g = g14
+                     g = g14
                 elif self.color == (0,0,255):
-                    g = g15
+                     g = g15
+                elif self.color == (255,165,0):
+                     g = g16
+                elif self.color == (0,255,255):
+                     g = g17
                 else:
-                    g = g16
-                
+                    g = g18
+
+            elif i.color == (255,165,0):
+                if self.color ==(255,0,0):
+                    g = g19
+                elif self.color == (0,255,0):
+                    g = g20
+                elif self.color == (0,0,255):
+                    g = g21
+                elif self.color == (255,165,0):
+                    g = g22
+                elif self.color == (0,255,255):
+                    g = g23
+                else:
+                    g = g24
+            elif i.color == (0,255,255):
+                if self.color ==(255,0,0):
+                    g = g25
+                elif self.color == (0,255,0):
+                    g = g26
+                elif self.color == (0,0,255):
+                    g = g27
+                elif self.color == (255,165,0):
+                    g = g28
+                elif self.color == (0,255,255):
+                    g = g29
+                else:
+                    g=30
+            else:
+                if self.color ==(255,0,0):
+                    g = g31
+                elif self.color == (0,255,0):
+                    g = g32
+                elif self.color == (0,0,255):
+                    g = g33
+                elif self.color == (255,165,0):
+                    g = g34
+                elif self.color == (0,255,255):
+                    g = g35
+                else:
+                    g = g36
                 
             if dist > 0:
                 F = g*.3/dist
@@ -156,27 +245,6 @@ class Bot:
         if self.y < 5:
             mY = mY * -1
                     
-            #if i.color == (255,0,0):
-             #   if self.x < i.x:
-             #       mX = mX + (dist/1000)
-
-             #   else:
-            #        mX = mX - (dist/1000)
-
-              #  if self.y < i.y:
-              #      mY = mY + (dist/1000)
-             #   else:
-             #       mY = mY - (dist/1000)
-           # else:
-            #    if self.x > i.x:
-             #       mX = mX + (dist/10000)
-            #    else:
-            #        mX = mX - (dist/10000)
-
-             #   if self.y > i.y:
-             #       mY = mY + (dist/10000)
-             #   else:
-              #      mY = mY - (dist/10000)
         self.x = self.x + mX
         self.y = self.y + mY
         if self.x < 15:
@@ -197,20 +265,8 @@ class Bot:
 
 
 
-for i in range(0,pinkBots):
-    bot = Bot(random.randint(1,1300), random.randint(1,800), (255,0,0)) 
-    bots.append(bot)
-
-for i in range(0,yellowBots):
-    bot = Bot(random.randint(1,1300), random.randint(1,800), (215, 215, 18)) 
-    bots.append(bot)
-
-for i in range(0,greenBots):
-    bot = Bot(random.randint(1,1300), random.randint(1,800), (0,255,0)) 
-    bots.append(bot)
-
-for i in range(0,blueBots):
-    bot = Bot(random.randint(1,1300), random.randint(1,800), (0,0,255)) 
+for _ in range(pinkBots + yellowBots + greenBots + blueBots + orangeBots + cyanBots):
+    bot = Bot(random.randint(1, 1300), random.randint(1, 800), random.choice([(255, 0, 0), (215, 215, 18), (0, 255, 0), (0, 0, 255), (255, 165, 0), (0, 255, 255)]))
     bots.append(bot)
     
 while True:
@@ -229,10 +285,8 @@ while True:
     if keys[pygame.K_r]:
         for i in bots:
             bots.remove(i)
-        pinkBots = random.randint(1, 45) 
-        yellowBots = random.randint(1, 45) 
-        greenBots = random.randint(1, 45) 
-        blueBots = random.randint(1, 45)
+  
+        cBots = random.randint(1, 5) 
 
         g1 = random_number = random.uniform(-1, 1)
         g2 = random_number = random.uniform(-1, 1)
@@ -243,28 +297,56 @@ while True:
         g7 = random_number = random.uniform(-1, 1)
         g8 = random_number = random.uniform(-1, 1)
         g9 = random_number = random.uniform(-1, 1)
-        g10= random_number = random.uniform(-1, 1)
+
+        g10 = random_number = random.uniform(-1, 1)
         g11 = random_number = random.uniform(-1, 1)
         g12 = random_number = random.uniform(-1, 1)
         g13 = random_number = random.uniform(-1, 1)
         g14 = random_number = random.uniform(-1, 1)
         g15 = random_number = random.uniform(-1, 1)
         g16 = random_number = random.uniform(-1, 1)
-        for i in range(0,pinkBots):
-            bot = Bot(random.randint(1,1300), random.randint(1,800), (255,0,0)) 
-            bots.append(bot)
 
-        for i in range(0,yellowBots):
-            bot = Bot(random.randint(1,1300), random.randint(1,800), (215, 215, 18)) 
-            bots.append(bot)
+        g10 = random_number = random.uniform(-1, 1)
+        g11 = random_number = random.uniform(-1, 1)
+        g12 = random_number = random.uniform(-1, 1)
+        g13 = random_number = random.uniform(-1, 1)
+        g14 = random_number = random.uniform(-1, 1)
+        g15 = random_number = random.uniform(-1, 1)
+        g16 = random_number = random.uniform(-1, 1)
 
-        for i in range(0,greenBots):
-            bot = Bot(random.randint(1,1300), random.randint(1,800), (0,255,0)) 
-            bots.append(bot)
-
-        for i in range(0,blueBots):
-            bot = Bot(random.randint(1,1300), random.randint(1,800), (0,0,255)) 
-            bots.append(bot)
+        g17 = random_number = random.uniform(-1, 1)
+        g18 = random_number = random.uniform(-1, 1)
+        g19 = random_number = random.uniform(-1, 1)
+        g20 = random_number = random.uniform(-1, 1)
+        g21 = random_number = random.uniform(-1, 1)
+        g22 = random_number = random.uniform(-1, 1)
+        g23 = random_number = random.uniform(-1, 1)
+        g24= random_number = random.uniform(-1, 1)
+        g25 = random_number = random.uniform(-1, 1)
+        g26 = random_number = random.uniform(-1, 1)
+        g27 = random_number = random.uniform(-1, 1)
+        g28 = random_number = random.uniform(-1, 1)
+        g29 = random_number = random.uniform(-1, 1)
+        g30 = random_number = random.uniform(-1, 1)
+        g31 = random_number = random.uniform(-1, 1)
+        g32 = random_number = random.uniform(-1, 1)
+        g33 = random_number = random.uniform(-1, 1)
+        g34 = random_number = random.uniform(-1, 1)
+        g35 = random_number = random.uniform(-1, 1)
+        g36 = random_number = random.uniform(-1, 1)
+        if cBots == 1:
+            for _ in range(120):
+                bot = Bot(random.randint(1, 1300), random.randint(1, 800), random.choice([(255, 0, 0), (215, 215, 18), (0, 255, 0), (0, 0, 255), (255, 165, 0), (0, 255, 255)]))
+                bots.append(bot)
+        elif cBots == 2:
+            for _ in range(120):
+                bot = Bot(random.randint(1, 1300), random.randint(1, 800), random.choice([(255, 0, 0), (0, 255, 0), (255, 165, 0), (0, 255, 255)]))
+                bots.append(bot)
+        else:
+            for _ in range(120):
+                bot = Bot(random.randint(1, 1300), random.randint(1, 800), random.choice([(0, 255, 0), (255, 165, 0), (0, 255, 255)]))
+                bots.append(bot)
+            
 
             
         
